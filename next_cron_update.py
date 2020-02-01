0f98bbd3f4a55bab5b0ae5558cffaf7db337d9d8
Konrad Harradine <agm94hln@gmail.com>
2020-02-01 22:11:34 +0000

    review changes 4

--- next_cron_update.py
+++ next_cron_update.py
@@ -5,7 +5,7 @@ Given the user's input time, the next time the cron jobs will be fired will
 be printed within terminal
 '''
 
-import sys
+from sys import argv, stdin
 import datetime as dt
 from croniter import croniter
 
@@ -23,9 +23,9 @@ def datetime_format(time_string):
     except:
         raise ValueError("Incorrect time format. Change to: HH:MM (E.g. 16:10)")
 
-def next_update():
+def print_next_updates(time_str):
     """Print the times and day when the cron jobs will be fired next, along with the file location."""
-    crontab_list = sys.stdin.readlines()
+    crontab_list = stdin.readlines()
 
     for i in range(len(crontab_list)):
         crontab_time = crontab_list[i].split()
@@ -33,8 +33,8 @@ def next_update():
         stdin_minuntes = crontab_time[0]
         stdin_hours = crontab_time[1]
 
-        curr_hour = datetime_format(sys.argv[1]).hour
-        curr_minutes = datetime_format(sys.argv[1]).minute
+        curr_hour = datetime_format(time_str).hour
+        curr_minutes = datetime_format(time_str).minute
 
         command_line_time = dt.datetime(curr_year,curr_month,curr_day,curr_hour,curr_minutes)
         next_time_datetime = croniter(f'{stdin_minuntes} {stdin_hours} * * *',command_line_time).get_next(dt.datetime)
@@ -49,15 +49,14 @@ def next_update():
 
 # Have the time string format checked, and if the format is correct then print cron times
 def main():
-    len_argv = len(sys.argv)
+    len_argv = len(argv)
 
-    if lev_argv != 2:
+    if len_argv != 2:
         print("Expected a single argument in HH:MM format, eg 16:10.")
         exit(1)
 
-    time_string = sys.argv[1]
-    if datetime_format(time_string):
-        next_update()
+    if datetime_format(argv[1]):
+        print_next_updates(argv[1])
 
 if __name__ == "__main__":
     main()
